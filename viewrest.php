b33fa7b3ac044eb5d9b7d86ce53d4bd1fa2a66a7
KaaWang <66406950+KaaWang@users.noreply.github.com>
2020-06-10 12:08:43 +0800

    Create viewrest.php

---
+++ viewrest.php
@@ -0,0 +1,13 @@
+<?php
+$connectionInfo = array("UID" => "apuadmin", "pwd" => "{Stormer32@}", 
+"Database" => "WebApplicationDB", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:webapplicationtp043517.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+if(!$conn)
+{
+die("Error connection: ".sqlsrv_errors());
+}
+echo "Connection to DB: Success!";
+
+?>
