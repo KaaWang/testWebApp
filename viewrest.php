250430c09de770de31a7f46a63c4dc7115c57ef7
KaaWang <66406950+KaaWang@users.noreply.github.com>
2020-06-10 12:14:21 +0800

    Update viewrest.php

--- viewrest.php
+++ viewrest.php
@@ -1,5 +1,5 @@
 <?php
-$connectionInfo = array("UID" => "apuadmin", "pwd" => "{Stormer32@}", 
+$connectionInfo = array("UID" => "apuadmin", "pwd" => "Stormer32@", 
 "Database" => "WebApplicationDB", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:webapplicationtp043517.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
